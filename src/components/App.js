8b99168820e00488e7aa6270700cc4f70b1111fd
valeria caliguire <caliguireval@gmail.com>
2023-05-09 14:53:51 -0600

    redo and forms

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,6 +6,29 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  //populates all of the db.json toys in state
+  const [toys, setToys] = useState([])
+
+
+
+
+//GET populates data from db.json
+useEffect(() => {
+  fetch("http://localhost:3001/toys")
+  .then(res => res.json())
+  .then(toys => setToys(toys))
+}, [])
+
+//copying the original object, and adding a new element
+const addToy = (toy) => {
+  setToys(
+    [...toys, toy]
+  )
+}
+
+
+
+
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
@@ -14,11 +37,11 @@ function App() {
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm addToy={addToy}/> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys}/>
     </>
   );
 }
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,22 +1,66 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ToyForm() {
+function ToyForm({addToy}) {
+
+  /**START FORM CODE**/
+const initialForm ={
+  name: "",
+  image: "",
+  likes: 0,
+}
+//form state
+const [form, setForm] = useState(initialForm)
+
+//CHANGE event handler
+function handleChange(e){
+  setForm({
+    ...form,
+    [e.target.name] : e.target.value
+  })
+}
+
+//SUBMIT event handler
+function handleSubmit(e){
+  e.preventDefault()
+//this is where a POST request goes because we want this to PERSIST
+  fetch(`http://localhost:3001/toys`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(form),
+    //why is this argument form and not body like in my notes?
+  })
+  .then(res => res.json()) //lastly, everything that is collected, please put it into JS we can read 
+  .then(data => {
+      addToy(data)
+      setForm(initialForm)
+})
+}
+
+
+/**END FORM CODE**/
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={(e) => handleSubmit(e)}>
         <h3>Create a toy!</h3>
         <input
+          value={form.name}
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          onChange={(e) => handleChange(e)}
         />
         <br />
         <input
+          value={form.image}
           type="text"
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          onChange={(e) => handleChange(e)}
         />
         <br />
         <input
